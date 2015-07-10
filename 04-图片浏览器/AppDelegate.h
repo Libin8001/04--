694e4788191fcb5e7c5b26d32293e383f10218a2
Lee <Lee@MacBook-Pro.local>
2015-07-10 15:38:57 +0800

    Initial Commit

---
+++ 04-图片浏览器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  04-图片浏览器
+//
+//  Created by Lee on 15/7/10.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 04-图片浏览器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  04-图片浏览器
+//
+//  Created by Lee on 15/7/10.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
